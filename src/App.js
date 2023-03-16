a98fad0f497d4cb1055416ff62397f6c8319551f
manjirimardikar <manjari.mardikar28@gmail.com>
2023-03-16 20:45:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,79 @@
-import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
+import { questions } from './Components/Questions'
+
+
+
+
 
 function App() {
+  const [currentvalue, setCurrentvalue] = useState(0)
+  const [showScore, setShowScore] = useState(false);
+  // const[btncolor, SetBtncolor] = useState("")
+  const [score, setScore]=useState(0)
+
+
+  const handleanswer=(isCorrect)=>{
+    if(isCorrect){
+      setScore(score+1)     
+    }
+    const nextvalue= currentvalue+1
+    if(nextvalue < questions.length){
+      setCurrentvalue(nextvalue)
+    }
+    else{
+      setShowScore(true)
+    }
+    
+    
+  }
+  const restartGame = () => {
+    setScore(0);
+    setCurrentvalue(0);
+    setShowScore(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      {showScore?(<div className='maincontainer'>
+        <h1>Final Results</h1>
+          <h2>
+            {score} out of {questions.length} correct - (
+            {(score / questions.length) * 100}%)
+          </h2>
+          <button className='restart-btn'onClick={() => restartGame()}>Restart game</button>
+  
+				</div>):(<>
+          <div className='maincontainer'>
+        <div className='header'>
+          <h1>Quiz Game</h1>
+          <h2>Current Score : {score}</h2>
+        </div>
+        <div className='question-section'>
+          <h2>Questions {currentvalue+1} out of {questions.length}</h2>
+          <h3> {questions[currentvalue].questionText}</h3>
+
+          <div className='answer-section'>
+          {questions[currentvalue].answerOptions.map((ele)=>{
+           return <button className='cont' onClick={()=>{
+            handleanswer(ele.isCorrect)
+            
+
+            
+           }}>{ele.answerText} </button>
+         
+
+          })}
+          </div>
+          
+         
+
+        </div>
+
+      </div>
+</>)}
+     
+    
     </div>
   );
 }
---
+++ src/Components/Questions.js
@@ -0,0 +1,47 @@
+export const questions = [
+    {
+        questionText: 'What is the capital of America?',
+        answerOptions: [
+            { answerText: 'New York City', isCorrect: false },
+            { answerText: 'Miami', isCorrect: false },
+            { answerText: 'Santa Fe', isCorrect: false },
+            { answerText: 'Washington DC', isCorrect: true },
+        ],
+    },
+    {
+        questionText: 'In which country is the Taj Mahal?',
+        answerOptions: [
+            { answerText: 'USA', isCorrect: false },
+            { answerText: 'UK', isCorrect: false },
+            { answerText: 'India', isCorrect: true },
+            { answerText: 'UAE', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'Who is CEO of Tesla?',
+        answerOptions: [
+            { answerText: 'Jeff Bezos', isCorrect: false },
+            { answerText: 'Elon Musk', isCorrect: true },
+            { answerText: 'Bill Gates', isCorrect: false },
+            { answerText: 'Tony Stark', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'The iPhone was created by which company?',
+        answerOptions: [
+            { answerText: 'Apple', isCorrect: true },
+            { answerText: 'Intel', isCorrect: false },
+            { answerText: 'Amazon', isCorrect: false },
+            { answerText: 'Microsoft', isCorrect: false },
+        ],
+    },
+    {
+        questionText: 'How many colors are there in a rainbow?',
+        answerOptions: [
+            { answerText: '1', isCorrect: false },
+            { answerText: '4', isCorrect: false },
+            { answerText: '6', isCorrect: false },
+            { answerText: '7', isCorrect: true },
+        ],
+    },
+];
